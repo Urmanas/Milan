9d68c34a0bdca65f71d05b7ff98e451c22a9941d
Urmanas <Urmanas.19@gmail.com>
2026-02-23 10:32:23 +0600

    Create a general page of website, add item app, images

--- core/views.py
+++ core/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'core/home.html')
+from item.models import Category, Item 
+
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    return render(request, 'core/index.html', {
+        'items': items,
+        'categories': categories,
+    })
+
+def contact(request):
+    return render(request, 'core/contact.html')
